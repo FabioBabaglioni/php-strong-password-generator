46c58f39c7152359a2def9ee4133d6ea7fb7bcc4
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-11 12:33:06 +0100

    Aggiunta di form

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pasword generator</title>
+</head>
+<body>
+    <h1>Strong pasword generator</h1>
+
+    <form action="">
+        <label for="number">Lunghezza password</label>
+        <input type="number" name="number" id="">
+        <input type="submit" value="Genera">
+    </form>
+</body>
+</html>
